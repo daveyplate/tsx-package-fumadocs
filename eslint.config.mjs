a02ee749da99fea4fe8efdb7223ed576e9be20cb
daveycodez <david@swiftapk.com>
2025-01-29 15:44:25 -0800

    next-eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,39 +1,24 @@
-import globals from "globals"
-import pluginJs from "@eslint/js"
-import tseslint from "typescript-eslint"
-import pluginReact from "eslint-plugin-react"
-import pluginReactHooks from "eslint-plugin-react-hooks"
-import reactHooksAddons from "eslint-plugin-react-hooks-addons"
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 import stylistic from "@stylistic/eslint-plugin"
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    settings: {
-      react: {
-        version: "detect"
-      }
-    },
-  },
-  {
-    ignores: ["node_modules/*", "dist/*", "src/components/ui/*",],
-  },
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } }, },
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  reactHooksAddons.configs.recommended,
-  ...tseslint.configs.recommended,
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     plugins: {
-      "react-hooks": pluginReactHooks,
       "@stylistic": stylistic,
     },
     rules: {
-      ...pluginReactHooks.configs.recommended.rules,
-      "react/react-in-jsx-scope": "off",
-      "@typescript-eslint/no-unused-vars": "off",
       "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-unused-vars": "off",
       "@stylistic/quotes": "warn",
       "@stylistic/jsx-newline": ["warn", { "prevent": true, "allowMultilines": true }],
       "@stylistic/jsx-max-props-per-line": ["warn", { "maximum": 1, "when": "multiline" }],
@@ -41,7 +26,9 @@ export default [
       "@stylistic/jsx-first-prop-new-line": "warn",
       "@stylistic/jsx-closing-bracket-location": "warn",
       "@stylistic/jsx-self-closing-comp": "warn",
-      "@stylistic/no-multiple-empty-lines": ["warn", { "max": 1, "maxEOF": 0 }],
-    },
+      "@stylistic/no-multiple-empty-lines": ["warn", { "max": 1, "maxEOF": 0 }]
+    }
   }
-]
+];
+
+export default eslintConfig;
