878728e7c2a2dc3bf6ff7c8b2845491751241e0c
daveycodez <david@swiftapk.com>
2025-01-29 20:16:42 -0800

    Update next.config.ts

--- docs/next.config.ts
+++ docs/next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
     /* config options here */
-    output: "export"
+    output: "export",
+    basePath: process.env.NODE_ENV == "production" ? "/my-tsx-package-fumadocs" : "",
+    images: { unoptimized: true }
 }
 
 export default nextConfig
