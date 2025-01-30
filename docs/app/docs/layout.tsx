748983bb4d72a283f4e5ede0ccc954b49503fdb4
daveycodez <david@swiftapk.com>
2025-01-29 23:25:24 -0800

    System toggle for theme, blue primary color

--- docs/app/docs/layout.tsx
+++ docs/app/docs/layout.tsx
@@ -1,12 +1,24 @@
 import { DocsLayout } from "fumadocs-ui/layouts/docs"
 import type { ReactNode } from "react"
+import { ThemeToggle } from "fumadocs-ui/components/layout/theme-toggle"
 
 import { baseOptions } from "@/app/layout.config"
 import { source } from "@/lib/source"
 
 export default function Layout({ children }: { children: ReactNode }) {
     return (
-        <DocsLayout tree={source.pageTree} {...baseOptions}>
+        <DocsLayout
+            tree={source.pageTree}
+            {...baseOptions}
+            disableThemeSwitch={true}
+            sidebar={{
+                footer: (
+                    <div className="md:-mt-8 w-32 ms-auto md:ms-0">
+                        <ThemeToggle mode="light-dark-system" />
+                    </div>
+                )
+            }}
+        >
             {children}
         </DocsLayout>
     )
