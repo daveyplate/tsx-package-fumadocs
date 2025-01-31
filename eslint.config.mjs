313134298c9df1cd4f2c48641d1ea4f8270bdedc
daveycodez <david@swiftapk.com>
2025-01-31 02:12:54 -0800

    Merged ESLint Configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,15 +9,11 @@ import importNewlinesPlugin from "eslint-plugin-import-newlines"
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-})
+const compat = new FlatCompat({ baseDirectory: __dirname, })
 
 const eslintConfig = [
     ...compat.extends("next/core-web-vitals", "next/typescript"),
-    {
-        ignores: ["dist", "out", "src/components/ui", "docs"],
-    },
+    { ignores: ["dist", "out", "src/components/ui", "docs"], },
     {
         ...importPlugin.flatConfigs.recommended,
         plugins: {
@@ -38,14 +34,14 @@ const eslintConfig = [
             "@stylistic/jsx-closing-tag-location": ["warn"],
             "@stylistic/jsx-self-closing-comp": "warn",
             "@stylistic/no-multiple-empty-lines": ["warn", { max: 1, maxEOF: 0, maxBOF: 0 }],
+            "@stylistic/function-paren-newline": ["warn", "multiline"],
             "@stylistic/padding-line-between-statements": ["warn",
                 { blankLine: "always", prev: "*", next: "return" },
                 { blankLine: "always", prev: "if", next: "*" },
-                { blankLine: "any", prev: "if", next: "return" },
                 { blankLine: "any", prev: "if", next: "if" },
                 { blankLine: "always", prev: "*", next: "if" },
                 { blankLine: "any", prev: ["const", "let", "var"], next: "*" },
-                { blankLine: "any", prev: ["*"], next: ["const", "let", "var"] },
+                { blankLine: "always", prev: ["*"], next: ["const", "let", "var"] },
                 { blankLine: "any", prev: ["const", "let", "var"], next: ["const", "let", "var"] },
                 { blankLine: "always", prev: "directive", next: "*" }, { blankLine: "any", prev: "directive", next: "directive" },
                 { blankLine: "always", prev: ["case", "default"], next: "*" }
